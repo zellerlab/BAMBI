ee6b169364e89eecb492eddefef54ec19bb11b5c
Jakob Wirbel <jakob.wirbel@gmail.com>
2022-09-17 17:26:31 -0700

    adjust resampling script as well.

--- create_simulations/sim_resampling.R
+++ create_simulations/sim_resampling.R
@@ -23,11 +23,13 @@ params <- yaml.load_file(here('create_simulations', "parameters.yaml"))
 # parameters
 feature.type <- c('all', 'low', 'middle', 'high', 'low_middle', 
                   'abundance', 'inverse-abundance')
-simulation <- paste0(temp.loc, 'simulations/sim_')
+simulation <- paste0(temp.loc, 'simulations/sim_Zeevi_WGS_')
 
 for (ft in feature.type){
   sim <- paste0(simulation, ft, '.h5')
   message(sim)
+  if (file.exists(here('simulations', 'resampling', 
+                       paste0('sim_Zeevi_WGS_', i, '.h5')))){next()}
   # create simulations
   create.data.simulation(feat = feat.zeevi,
                 meta = meta.zeevi,
@@ -54,7 +56,7 @@ for (ft in feature.type){
 
 
 # additionally create un-balanced implantation for compositional effects
-sim <- paste0(simulation, 'compositional.h5')
+sim <- paste0(temp.loc, 'sim_Zeevi_WGS_compositional.h5')
 
 # create simulations
 create.data.simulation(feat = feat.zeevi,
@@ -77,3 +79,35 @@ create.test.idx(sim.location=sim,
                 subsets=params$subset_size,
                 repetitions=50)
 file.move(sim, here('simulations', 'resampling'))
+
+# Other datasets
+for (i in names(dataset.list)[-1]){
+  sim <- paste0(temp.loc, 'sim_', i, '_all.h5')
+  message(sim)
+  if (file.exists(here('simulations', 'resampling', 
+                       paste0('sim_', i, '_all.h5')))){next()}
+  # create simulations
+  create.data.simulation(
+    feat = data.list[[i]]$feat,
+    meta = data.list[[i]]$meta,
+    sim.location = sim,
+    sim.type='cross-section',
+    sim.method='resampling',
+    filt.params = list(ab.cutoff=ifelse(str_detect(i, 'KEGG'), 1e-07,
+                                        as.numeric(params$ab.cutoff)),
+                       prev.cutoff=params$prev.cutoff,
+                       log.n0=as.numeric(params$log.n0)),
+    sim.params = list(ab.scale=params$ab.scale,
+                      prev.scale=params$prev.scale,
+                      prop.markers=params$prop.markers,
+                      conf=NULL,
+                      class.balance=params$class.balance,
+                      feature.type='all',
+                      repeats=50))
+  
+  # create testing indices
+  create.test.idx(sim.location=sim,
+                  subsets=params$subset_size,
+                  repetitions=50)
+  file.move(sim, here('simulations', 'resampling'))
+}
