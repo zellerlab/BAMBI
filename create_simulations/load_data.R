9badeda50b246ee27832bc9a98201a3892964edb
Jakob Wirbel <jakob.wirbel@gmail.com>
2024-08-23 12:07:57 +0200

    we do not include milieu interieur after all.

--- create_simulations/load_data.R
+++ create_simulations/load_data.R
@@ -71,34 +71,10 @@ rownames(meta.schirmer) <- meta.schirmer$Sample_ID
 meta.schirmer$Individual_ID <- meta.schirmer$Sample_ID
 meta.schirmer$Timepoint <- 0
 
-# ##############################################################################
-# Milieu interieur
-fn.feat <- here('data', 'motus_MilieuInterieur_WGS.tsv')
-feat.mi.wgs <- read.table(fn.feat, sep='\t', stringsAsFactors = FALSE,
-                       check.names = FALSE, quote = '', comment.char = '',
-                       row.names = 1, header = TRUE)
-fn.meta <- here('data', 'meta_MilieuInterieur_WGS.tsv')
-meta.mi.wgs <- read_tsv(fn.meta, col_types = cols()) %>%
-  filter(!is.na(Sample_ID)) %>%
-  as.data.frame()
-rownames(meta.mi.wgs) <- meta.mi.wgs$Sample_ID
-# 16S
-fn.feat <- here('data', 'otus_MilieuInterieur_16S.tsv')
-feat.mi.16S <- read.table(fn.feat, sep='\t', stringsAsFactors = FALSE,
-                          check.names = FALSE, quote = '', comment.char = '',
-                          row.names = 1, header = TRUE)
-fn.meta <- here('data', 'meta_MilieuInterieur_16S.tsv')
-meta.mi.16s <- read_tsv(fn.meta, col_types = cols()) %>%
-  filter(!is.na(Sample_ID)) %>%
-  as.data.frame()
-rownames(meta.mi.16s) <- meta.mi.16s$Sample_ID
-
 
 dataset.list <- list(
   'Zeevi_WGS'=list('feat'=feat.zeevi, 'meta'=meta.zeevi),
   'Schirmer_WGS'=list('feat'=feat.schirmer, 'meta'=meta.schirmer),
   'TwinsUK_WGS'=list('feat'=feat.xie, 'meta'=meta.xie),
   'TwinsUK_16S'=list('feat'=feat.goodrich, 'meta'=meta.goodrich),
-  'MI_WGS'=list('feat'=feat.mi.wgs, 'meta'=meta.mi.wgs),
-  'MI_16S'=list('feat'=feat.mi.16S, 'meta'=meta.mi.16s),
   'Zeevi_KEGG'=list('feat'=feat.zeevi.kegg, 'meta'=meta.zeevi))
