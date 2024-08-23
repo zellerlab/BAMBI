92562111509d92cd082784e35f5e8c9519e21698
Jakob Wirbel <jakob.wirbel@gmail.com>
2024-08-23 12:23:32 +0200

    minor updates for the compositionality-confounded simulation extra figure.

--- figures/src/figure_comp.R
+++ figures/src/figure_comp.R
@@ -11,119 +11,104 @@ library("ggembl")
 library("ggrepel")
 
 # ##############################################################################
-# AUC
-auc.comp <- read_tsv(here('reality_checks', 'compositional', 'auc_all.tsv'))
-auc.rest <- read_tsv(here('reality_checks', 'auc_all.tsv'))
-
-g <- auc.comp %>% 
-  bind_rows(auc.rest) %>% 
-  filter(type=='ML') %>% 
-  filter(simulation %in% c('sim_compositional', 'sim_all', 
-                           'sim_MMH', 'sim_negbin')) %>% 
-  ggplot(aes(x=simulation, y=AUC, fill=as.factor(ab))) + 
-  geom_boxplot() +
-  theme_publication() + 
-  ylab("log10(F value)") +
-  scale_fill_manual(values=viridis::viridis(n=7), name='Abundance')
-
-
-auc.values.rest <- c(1, 1.25, 1.5, 2, 5, 10, 20)
-auc.values.comp <- c(1, 1.25, 1.5, 2, 2.5, 3, 3.5, 4, 5, 10, 20)
-g <- auc.comp %>% 
-  mutate(ab.value=auc.values.comp[ab]) %>% 
-  bind_rows(auc.rest %>% 
-              mutate(ab.value=auc.values.rest[ab])) %>% 
-  filter(type=='Euclidean') %>% 
-  filter(simulation %in% c('compositional', 'sim_all', 
-                           'sim_MMH', 'sim_negbin')) %>% 
-  mutate(prev=paste0('prev_', prev)) %>% 
-  ggplot(aes(x=simulation, y=log10(F_value + 1), fill=as.factor(ab.value))) + 
-    geom_boxplot() +
-    theme_publication() + 
-    ylab("log10(F value)") +
-    scale_fill_manual(values=viridis::viridis(n=11), name='Abundance') +
-    NULL
-ggsave(g, filename = here('figures', 'figure_comp', 'auc.pdf'),
-       width = 7, height = 4, useDingbats=FALSE)
-
-# ##############################################################################
 # effect size plot
 
-effect.size.all <- read_tsv(here('reality_checks', 'sim_all', 'variance.tsv'))
+effect.size.all <- read_tsv(here('reality_checks', 'sim_Zeevi_WGS_all', 
+                                 'variance.tsv'))
 effect.size.comp <- read_tsv(here('reality_checks', 
-                                  'compositional', 'variance.tsv'))
+                                  'sim_Zeevi_WGS_compositional', 
+                                  'variance.tsv'))
 
+# part A: shift in background feature effect size
 g <- effect.size.all %>% 
-  filter(prev==2) %>% 
-  filter(rep==48) %>% 
-  bind_rows(effect.size.comp %>% filter(prev==2) %>% filter(rep==48)) %>% 
+  bind_rows(effect.size.comp) %>% 
+  filter(prev==2, rep==89) %>% 
   ggplot(aes(x=as.factor(ab), y=fc.sim, fill=selection)) + 
     geom_boxplot(outlier.shape = NA) + 
-    geom_jitter(position = position_jitterdodge(jitter.width = 0.08),
-                size=0.7) + 
-    facet_grid(~simulation, scales = 'free_x', space = 'free') +
+    geom_jitter(position = position_jitterdodge(jitter.width = 0.1),
+                size=0.7, aes(col=selection)) + 
+    facet_grid(~simulation) + 
     theme_publication(panel.grid = 'major_y') + 
     xlab('Abundance scaling') + 
     ylab('Generalized fold change') + 
-    scale_fill_manual(values=alpha(c('#707372', '#3B6FB6'), alpha=0.7), 
-                      name='Implanted')
+    scale_fill_manual(values=alpha(c('#707372', '#A8C700'), alpha=0.4), 
+                      name='Implanted') +
+    scale_colour_manual(values=c('#707372', '#A8C700'), name='Implanted')
 ggsave(g, filename = here('figures', 'figure_comp', 'effect_size.pdf'),
-       width = 7, height = 4, useDingbats=FALSE)
-
+       width = 6, height = 4, useDingbats=FALSE)
 
 # ##############################################################################
 # results from vanilla testing
 
 # take a specific group/sample size
 fn.final <- here('figures', 'figure_comp', 'test_results.tsv')
-if (!file.exists(fn.test.results)){
-  fn.test.results <- here('test_results', 'compositional_all.tsv')
-  df.res.all <- read_tsv(fn.test.results) %>% 
-    mutate(group=str_remove(group, '_rep[0-9]*$')) %>%
-    mutate(subset=str_remove(subset, 'subset_')) %>% 
-    group_by(test, group, subset) %>% 
-    summarise(mean.FDR=mean(FDR), sd.FDR=sd(FDR),
-              mean.R=mean(R), sd.R=sd(R),
-              mean.auroc=mean(auroc), sd.auroc=sd(auroc)) %>% 
-    separate(group, into=c('ab', 'prev'), sep='_')
-  write_tsv(df.res.all, file=fn.final)
+if (!file.exists(fn.final)){
+  .f_read_single_test <- function(fn, fn.final){
+    message(fn)
+    res <- vroom::vroom(fn) %>% 
+      separate(group, into=c('group', 'repetition'), 
+               sep='_rep') %>% 
+      mutate(subset=str_remove(subset, 'subset_')) %>% 
+      mutate(subset=as.factor(as.numeric(subset))) %>% 
+      mutate(PR = 1-FDR) %>% 
+      mutate(FPR=FP/(FP+TN)) %>% 
+      mutate(norm=case_when(test=='ZIBSeq-sqrt'~paste0(norm, '-sqrt'), 
+                            TRUE~norm)) %>% 
+      mutate(norm=case_when(norm=='pass-sqrt'~'sqrt', TRUE~norm)) %>% 
+      mutate(test=str_remove(test, '-sqrt$')) %>% 
+      select(-problem, -time.running)
+    res.combined <- res %>% 
+      group_by(group, subset, test, norm, adjust) %>% 
+      summarise(precision=mean(PR), sd.precision=sd(PR),
+                recall=mean(R), sd.recall=sd(R),
+                AUC=mean(auroc), sd.AUC=sd(auroc),
+                fdr=mean(FDR), sd.fdr=sd(FDR),
+                fpr=mean(FPR), sd.fpr=sd(FPR),
+                n.obs=n(),
+                .groups='drop') %>% 
+      separate(group, into=c('ab','prev'), 
+               sep = '_', remove=FALSE) %>% 
+      mutate(type=paste0(test, '-', norm))
+    # fix ANCOM
+    # message(length(unique(res.combined$test)))
+    if (unique(res.combined$test)=='ANCOM'){
+      res.combined <- res.combined %>% filter(adjust=='none')
+    }
+    write_tsv(res.combined, file=fn.final, append = file.exists(fn.final))
+  }
+  
+  fn.test.results <- list.files(here('test_results'), 
+                                pattern='compositional.*.tsv',
+                                full.names = TRUE, recursive = TRUE)
+  df.res.all <- map(fn.test.results, .f=.f_read_single_test, fn.final=fn.final)
 } else {
   df.res.all <- read_tsv(fn.final)
 }
 
 test.colours <- yaml::yaml.load_file(here('files', 'test_colours.yml'))
+params <- yaml::yaml.load(here('create_simulations', 'parameters.yaml'))
 
 g <- df.res.all %>% 
-  mutate(ab=factor(ab, levels = paste0('ab', seq_len(11)))) %>% 
-  filter(prev=='prev2') %>% 
-  select(test, ab, mean.FDR, mean.R, mean.auroc, subset) %>% 
-  mutate(mean.PR=1-mean.FDR) %>% 
-  pivot_longer(cols=c(mean.FDR, mean.R, mean.auroc, mean.PR)) %>% 
-  filter(name!='mean.FDR') %>% 
-  mutate(lt=test=='metagenomeSeq2') %>% 
+  bind_rows(df.res.all %>% 
+              filter(test=='ANCOM') %>% 
+              mutate(adjust='BH')) %>% 
+  filter(prev=='prev2', subset==200) %>% 
+  mutate(g=paste0(test, '-', norm)) %>% 
+  filter(g %in% c('ALDEx2-pass', 'ANCOM-rarefy', 'ANCOMBC-pass', 'corncob-pass', 
+                  'DESeq2-rarefy', 'distinct-pass', 'edgeR-pass', 'fastANCOM-pass', 
+                  'KS-clr', 'LDM-pass', 'limma-TSS.log', 'LinDA-pass', 'lm-TSS.log', 
+                  'metagenomeSeq-pass', 'metagenomeSeq2-pass', 'wilcoxon-TSS', 
+                  'ZIBSeq-sqrt', 'ZicoSeq-pass', 'ZINQ-pass', 
+                  't-test-TSS.log')) %>% 
+  dplyr::select(ab, test, precision, recall, AUC, fpr, adjust) %>% 
+  mutate(test=factor(test, levels = names(test.colours))) %>% 
+  mutate(fpr=1-fpr) %>% 
+  pivot_longer(c(precision, recall, AUC, fpr)) %>% 
   ggplot(aes(x=ab, y=value)) + 
-    geom_line(aes(group=test, col=test, lty=lt))  +
-    facet_grid(subset~name) +
-    xlab('Abundance scaling') + 
-    ylab('') +
-    scale_x_discrete(labels=c('1', '1.25', '1.5', '2', '2.5', '3', '3.5', 
-                              '4', '5', '10', '20')) +
-    theme_publication(panel.grid = 'major_y') + 
-    scale_colour_manual(values = unlist(test.colours))
+    geom_line(aes(group=test, colour=test)) +
+    scale_colour_manual(values=unlist(test.colours)) +
+    facet_grid(adjust~name, scales = 'free_y') + 
+    theme_presentation(panel.grid = 'major_y')
     
-ggsave(g, filename = here('figures', 'figure_comp', 'all_measures.pdf'),
+ggsave(g, filename = here('figures', 'figure_comp', 'all_measures_missing_2.pdf'),
        width = 5, height = 4, useDingbats=FALSE)
-
-
-
-df.res.all %>% 
-  mutate(ab=factor(ab, levels = paste0('ab', seq_len(11)))) %>% 
-  arrange(ab) %>% 
-  filter(prev=='prev2') %>% 
-  select(test, ab, mean.FDR, mean.R, mean.auroc, subset) %>% 
-  filter(subset==400) %>% 
-  mutate(mean.PR=1-mean.FDR) %>% 
-  ggplot(aes(x=mean.R, y=mean.PR, col=test)) + 
-    geom_point(aes(size=ab)) + 
-    geom_line(aes(group=test)) + 
-    scale_colour_manual(values = unlist(test.colours))
